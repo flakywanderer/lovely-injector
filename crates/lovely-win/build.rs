92ddfc6d49796e879cabf9919fac7c13f72afd0a
flakywanderer <flakywanderer@gmail.com>
2024-07-24 16:35:41 -0400

    Make stuff easier for me

--- crates/lovely-win/build.rs
+++ crates/lovely-win/build.rs
@@ -1,4 +1,8 @@
+use std::env;
+
 fn main() {
-    #[cfg(target_os = "windows")]
-    forward_dll::forward_dll("C:\\Windows\\System32\\version.dll").unwrap();
+    let home_dir = env::var("HOME").unwrap();
+    //#[cfg(target_os = "windows")]
+    //forward_dll::forward_dll("C:\\Windows\\System32\\version.dll").unwrap();
+    forward_dll::forward_dll(&(home_dir + "/Downloads/version-orig.dll")).unwrap();
 }
